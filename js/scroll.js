92b2a89a4431579ec4ed5ca222a61e7fe0067555
Juan Cetina <juan.cetina@correo.uis.edu.co>
2021-07-10 10:49:39 -0500

    inicializamos el proyecto portafolio

---
+++ js/scroll.js
@@ -0,0 +1,18 @@
+//Navbar
+window.addEventListener("scroll", function(){
+    var header = this.document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+})
+
+//Funcion click responsive barra
+$(document).ready(function(){
+    $('.menu-toggle').click(function(){
+        $('nav').toggleClass('active')
+    })
+})
+
+//
+const nav = document.querySelector('.menu-toggle button');
+nav.addEventListener('click', e =>{
+    nav.classList.toggle('open');
+})
